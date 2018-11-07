804f8e12d41d400467ee598c4ee26e9a8769ac15
Tor Gammelgård <tor.gammelgard@gmail.com>
2018-11-07 14:34:11 +0100

    Fix structure to fit gradle java plugin structure

--- src/main/java/PieChartController.java
+++ src/main/java/PieChartController.java
@@ -1,5 +1,3 @@
-package main;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- src/main/java/PieChartView.java
+++ src/main/java/PieChartView.java
@@ -1,5 +1,3 @@
-package main;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
